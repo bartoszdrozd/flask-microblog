740890d8d9b3d2d6e2e51006ff50d809c5769c68
Bartosz Drozd <bartosz.drozd94@gmail.com>
2021-06-04 23:02:08 +0200

    email notify

--- report_test.sh
+++ report_test.sh
@@ -2,5 +2,5 @@ export MAIL_SERVER=smtp.googlemail.com
 export MAIL_PORT=587
 export MAIL_USE_TLS=1
 export MAIL_USERNAME=error.logger.test@gmail.com
-export MAIL_PASSWORD= #<haslooooo>
-flask run
+export MAIL_PASSWORD=testtest.123 #password 
+#flask run
