b495c7119ee4091e6231cea988d46136e6667dfc
Bartosz Drozd <bartosz.drozd94@gmail.com>
2021-06-05 10:23:26 +0200

    reset password form

--- app/routes.py
+++ app/routes.py
@@ -3,9 +3,9 @@ from app import app, db
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User, Post
 from werkzeug.urls import url_parse
-from app.forms import RegistrationForm, LoginForm, EditProfileForm, EmptyForm, PostForm
+from app.forms import RegistrationForm, LoginForm, EditProfileForm, EmptyForm, PostForm, ResetPasswordRequestForm
 from datetime import datetime
-
+from app.email import send_password_reset_email
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -154,4 +154,17 @@ def explore():
     return render_template("index.html", title='Explore', posts=posts.items,
                           next_url=next_url, prev_url=prev_url)
 
+@app.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_password_reset_email(user)
+        flash('Check your email for the instructions to reset your password')
+        return redirect(url_for('login'))
+    return render_template('reset_password_request.html',
+                           title='Reset Password', form=form)
     
