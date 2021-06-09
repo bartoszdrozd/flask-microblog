af3178d9e6a276e2bc03d56905d0f54c29eb858e
Bartosz Drozd <bartosz.drozd94@gmail.com>
2021-06-09 17:05:21 +0200

    translations

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
     'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
@@ -15,4 +14,4 @@ class Config(object):
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['bartosz.drozd94@gmail.com', 'error.logger.test@gmail.com'] #mails which get the error messages
     POSTS_PER_PAGE = 25
-    LANGUAGES = ['en', 'es']
+    LANGUAGES = ['en', 'es', 'de', 'pl']
